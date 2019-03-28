1fc0a596d804e55f113310ce6170a6a1ab417574
glonpl <bratajczyk@o2.pl>
2019-03-28 18:54:52 +0100

    changed mind

---
+++ src/main/java/Game/Board.java
@@ -0,0 +1,8 @@
+package Game;
+
+public class Board {
+    private int height = 6;
+    private int width = 7;
+    public int[][] board = new int[height][width];
+    private int winner = 0;
+}
---
+++ src/test/java/Game/BoardTest.java
@@ -0,0 +1,29 @@
+package Game;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class BoardTest {
+
+@test
+public void LoadMapFromFile_isTypeOf2DIntArr(){
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+}
