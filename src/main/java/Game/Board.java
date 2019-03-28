cffc418dda5fa0346479149ab030ad382319645a
glonpl <bratajczyk@o2.pl>
2019-03-28 19:18:43 +0100

    tdd

--- src/main/java/Game/Board.java
+++ src/main/java/Game/Board.java
@@ -4,5 +4,13 @@ public class Board {
     private int height = 6;
     private int width = 7;
     public int[][] board = new int[height][width];
-    private int winner = 0;
+
+    public void createBoard() {
+        for(int i=0; i<height; i++) {
+            for(int j=0; j<width; j++) {
+                board[i][j] = 0;
+            }
+        }
+    }
+
 }
--- src/test/java/Game/BoardTest.java
+++ src/test/java/Game/BoardTest.java
@@ -6,13 +6,37 @@ import static org.hamcrest.Matchers.*;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class BoardTest {
 
-@test
-public void LoadMapFromFile_isTypeOf2DIntArr(){
+    private Board game;
 
+    @BeforeEach
+    public void setUp() {
+        game = new Board();
+    }
+
+
+
+
+    @Test
+    @DisplayName("Default board test")
+    public void CreateBoardTest() {
+        game.createBoard();
+
+      assertTrue(game.board.length == 6 && game.board[0].length == 7 );
+
+    }
+    @Test
+    @DisplayName("Prints 0 filled board test")
+    public void CreateBoard0FillPrintTest() {
+        game.createBoard();
+
+        assertEquals(game.board.printBoard," sad " );
+
+    }
 
 
 
@@ -26,4 +50,4 @@ public void LoadMapFromFile_isTypeOf2DIntArr(){
 
 
 
-}
+
